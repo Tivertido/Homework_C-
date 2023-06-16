1a2bcdb15c41037ebb6191c96dee78b4b8f5bd7c
Tema <Tema>
2023-06-16 20:32:07 +0300

    commit_sem3

---
+++ sem3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти: ");
+int a = int.Parse(Console.ReadLine()!);
+
+switch (a)
+{
+    case 1:
+    {
+        Console.WriteLine("В данной четверти х>0 и у >0.");      
+        break;
+    }
+     case 2:
+    {
+        Console.WriteLine("В данной четверти х<0 и у>0.");      
+        break;
+    }
+     case 3:
+    {
+        Console.WriteLine("В данной четверти х<0 и у<0.");      
+        break;
+    }
+     case 4:
+    {
+        Console.WriteLine("В данной четверти х>0 и у<0.");      
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Это значение вне диапазона");
+        break;
+    }
+}
