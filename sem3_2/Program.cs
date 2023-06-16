7f370ce63e6ed221c75fbe928026c06bfa4be45c
Tema <Tema>
2023-06-16 20:55:45 +0300

    добавлено описание задачи 3

---
+++ sem3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= number; i++)
+{
+    int sqr = Convert.ToInt32(Math.Pow(i, 2));
+    Console.WriteLine(sqr);
+}
